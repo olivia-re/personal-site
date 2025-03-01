313cfadab5d127818ab71f7d20ce95dc6decb737
olivia-re <116469447+olivia-re@users.noreply.github.com>
2025-03-01 18:24:09 -0500

    Implemented a reactbits component - downgraded to React 18

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import "@mantine/core/styles.css";
+import "./styles/global.css";
 import { MantineProvider } from "@mantine/core";
 import { HeaderSimple } from "./components/Header/HeaderSimple.tsx";
 import { TimelineTree} from "./components/Timeline/TimelineTree.tsx"
 import { Name } from "./components/Name/Name.tsx";
+import SplitText from "./components/TextAnimations/SplitText/SplitText";
+
+const handleAnimationComplete = () => {
+  console.log('All letters have animated!');
+};
 
 export default function App() {
   return(
     <MantineProvider >
       <HeaderSimple />
       <Name/>
+      <SplitText
+  text="Hello, Tailwind!"
+  className="text-2xl font-semibold text-center"
+  delay={150}
+  animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+  animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+  easing="easeOutCubic"
+  threshold={0.2}
+  rootMargin="-50px"
+  onLetterAnimationComplete={handleAnimationComplete}
+/>
       <TimelineTree />
       
     </MantineProvider>
--- src/components/Name/Name.tsx
+++ src/components/Name/Name.tsx
@@ -1,9 +1,13 @@
+import { useSpring, animated } from '@react-spring/web';
 
 export function Name(){
+
+    const props = useSpring({ from: { opacity: 0 }, to: { opacity: 1 }, config: { duration: 2000 } });
+
     return(
         <>
 <h1>Hello, my name is: </h1>
-<h2>Olivia Rego</h2>
+<animated.div style={props}>Olivia Rego</animated.div>;
 </>
     )
 }
